e66fc82f8e26814caa407c3280fd33a06cfe03c1
cuimengru <442402330@qq.com>
2020-06-09 10:36:26 +0000

    authRequest and logout

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,3 +6,20 @@ export function login(data) {
     data: data
   })
 }
+
+export function refresh(token) {
+  return request('authorizations/current', {
+    method: 'put',
+    header: {
+      'Authorization': 'Bearer ' + token
+    }
+  })
+}
+
+export function logout(token) {
+  return request('authorizations/current', {
+    method: 'delete',
+    header: {
+      'Authorization': 'Bearer ' + token
+    }
+  })
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,10 @@
+import { login, logout, refresh } from '@/api/auth'
+import { getCurrentUser } from '@/api/user'
 import wepy from '@wepy/core'
 import { login } from '@/api/auth'
 import * as auth from '@/utils/auth'
 import isEmpty from 'lodash/isEmpty'
+import { login, refresh } from '@/api/auth'
 
 const getDefaultState = () => {
   return {
@@ -31,6 +34,29 @@ const actions = {
 
     commit('setToken', authResponse.data)
     auth.setToken(authResponse.data)
+
+    dispatch('getUser')
+  },
+  async getUser ({ dispatch, commit }) {
+    const userResponse = await getCurrentUser()
+
+    commit('setUser', userResponse.data)
+    auth.setUser(userResponse.data)
+  },
+  async refresh ({ dispatch, commit, state }, params = {}) {
+    const refreshResponse = await refresh(state.accessToken, {}, false)
+
+    commit('setToken', refreshResponse.data)
+    auth.setToken(refreshResponse.data)
+
+    dispatch('getUser')
+  },
+  async logout ({ commit, state }) {
+    await logout(state.accessToken)
+
+    // 清空 storage
+    auth.logout()
+    commit('resetState')
   }
 }
 
@@ -42,6 +68,9 @@ const mutations = {
   setToken(state, tokenPayload) {
     state.accessToken = tokenPayload.access_token
     state.accessTokenExpiredAt = new Date().getTime() + tokenPayload.expires_in * 1000
+  }
+  resetState: (state) => {
+    Object.assign(state, getDefaultState())
   }
 }
 
