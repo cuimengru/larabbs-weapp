0af83571672522a0ea6d24373e5e032854291530
cuimengru <442402330@qq.com>
2020-06-10 05:47:50 +0000

    page notification index

--- src/api/notification.js
+++ src/api/notification.js
@@ -1,5 +1,11 @@
 import { authRequest } from '@/utils/request'
 
-export function getNotificationStats(...params) {
-  return authRequest('notifications/stats', ...params)
+export function getNotifications() {
+  return authRequest('notifications')
+}
+
+export function readNotifications() {
+  return authRequest('user/read/notifications', {
+    method: 'PUT'
+  })
 }
--- src/store/modules/notification.js
+++ src/store/modules/notification.js
@@ -1,6 +1,7 @@
 import wepy from '@wepy/core'
 
-import { getNotificationStats } from '@/api/notification'
+import { getNotificationStats, readNotifications } from '@/api/notification'
+
 
 const state = {
   unreadCount: 0
@@ -30,6 +31,13 @@ const mutations = {
     state.unreadCount = count
   }
 }
+
+ async readNotifications ({ commit }, params = {}) {
+    const statsResponse = await readNotifications()
+
+    commit('setUnreadCount', 0)
+  }
+  
 
 export default {
   state,
