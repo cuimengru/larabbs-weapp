e7de4caaa0014ae6d27d10c90e438f396c1b12a5
cuimengru <442402330@qq.com>
2020-06-10 01:44:23 +0000

    update avatar

--- src/api/user.js
+++ src/api/user.js
@@ -1,3 +1,4 @@
+import { request, authRequest, uploadFile } from '@/utils/request'
 import { request, authRequest } from '@/utils/request'
 
 export function getCurrentUser(data) {
@@ -8,5 +9,16 @@ export function updateUser(data) {
   return authRequest('user', {
     method: 'put',
     data: data
+  })
+}
+
+export function updateAvatar(avatar) {
+  return uploadFile('images', {
+    method: 'POST',
+    name: 'image',
+    formData: {
+      type: 'avatar'
+    },
+    filePath: avatar
   })
 }
