49f4daf54c85ff005ae38e0dbe9af21e6221d7e9
cuimengru <442402330@qq.com>
2020-06-10 05:33:12 +0000

    unread notification

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vuex from '@wepy/x';
 import users from './modules/user'
+import notification from './modules/notification'
 
 export default new Vuex.Store({
-   modules: {
-    users,
+  // getters,
+  modules: {
+    user,
+    notification
   }
   /*state: {
     counter: 0
